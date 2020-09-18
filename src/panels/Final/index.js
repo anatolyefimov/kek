0b6f28cd090a351267ba3786df3e46ce8c51fd47
Viacheslav Panasovets <pansslava@gmail.com>
2020-09-18 12:14:05 +0300

    finished final screen

--- src/panels/Final/index.js
+++ src/panels/Final/index.js
@@ -10,23 +10,24 @@ import {
 import Icon56AddCircleOutline from '@vkontakte/icons/dist/56/add_circle_outline';
 
 import './style.css'
+import { Icon56CheckCircleOutline } from "@vkontakte/icons";
 
 const Home = ({ id, go }) => (
   <Panel id={id} centered>
     <PanelHeader>Подкасты</PanelHeader>
     <Placeholder
-      icon={ <Icon56AddCircleOutline className='Home__icon'/> }
-      header='Добавьте первый подкаст'
+      icon={ <Icon56CheckCircleOutline style={{color: '#3F8AE0'}} className='Home__icon'/> }
+      header='Подкаст добавлен'
       action={
         <Button
           size='l'
         >
-          Подкаст добавлен
+          Поделиться подкастом
         </Button>
       }
       className='Home__placeholder'
     >
-      Добавляйте, редактируйте и делитесь подкастами вашего сообщества.
+      Раскажите своим подписчикам о новом подкасте, чтобы получить больше слушателей.
     </Placeholder>
   </Panel>
 );
