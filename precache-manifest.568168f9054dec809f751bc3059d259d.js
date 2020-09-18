1153d17c72d832153238b14c367838c1d500c890
anatolyefimov <efimo.tolik@gmail.com>
2020-09-18 08:33:15 +0300

    Updates

---
+++ precache-manifest.568168f9054dec809f751bc3059d259d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c78fa402c79d72e5d64f6a4107b6b9b4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fe5adb419631aa31aad6",
+    "url": "/static/css/2.b45f4e43.chunk.css"
+  },
+  {
+    "revision": "1e3f4c353f07f94e24dc",
+    "url": "/static/css/main.29f2b9ef.chunk.css"
+  },
+  {
+    "revision": "fe5adb419631aa31aad6",
+    "url": "/static/js/2.17704d3b.chunk.js"
+  },
+  {
+    "revision": "64b63cd42c989336db11595ed8b01ec6",
+    "url": "/static/js/2.17704d3b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e3f4c353f07f94e24dc",
+    "url": "/static/js/main.c26119bb.chunk.js"
+  },
+  {
+    "revision": "3b3741d10aca7bfa79db",
+    "url": "/static/js/runtime-main.4f11f408.js"
+  },
+  {
+    "revision": "918c2155014bca5690366d23357d83f8",
+    "url": "/static/media/double-slider-thumb.918c2155.svg"
+  },
+  {
+    "revision": "9fd923f193b03d35db174e756ab59fa7",
+    "url": "/static/media/slider_thumb.9fd923f1.svg"
+  }
+]);
